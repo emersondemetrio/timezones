d8aeebb8e72dea0656dd4a1d75bae848f88b7e2f
Emerson Demetrio <emerson@drivekyte.com>
2022-03-04 09:40:25 +0100

    add json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import timeZones from './TIMEZONES.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const genericDateOptions = {
+	year: 'numeric',
+	month: 'numeric',
+	day: 'numeric',
+	hour: 'numeric',
+	minute: 'numeric',
+	second: 'numeric',
+	hour12: false,
+};
 
-export default App;
+const formatDate = (timeZone, date) =>
+	new Intl.DateTimeFormat('pt-BR', {
+		...genericDateOptions,
+		timeZone,
+	}).format(date);
+
+const Folks = ({ list }) => {
+	return (
+		<ul>
+			{list.map((l) => (
+				<li key={l}>{l}</li>
+			))}
+		</ul>
+	);
+};
+
+const TimeZone = ({ name, timeZone, folks }) => {
+	const [tz, setTz] = useState(formatDate(timeZone, new Date()));
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTz(formatDate(timeZone, new Date()));
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, [timeZone]);
+
+	return (
+		<div className="card">
+			<h4>{name}</h4>
+			<h4>{tz}</h4>
+		</div>
+	);
+};
+
+export const App = () => {
+	return (
+		<div className="App">
+			<h3>Timezones</h3>
+			<div className="main-container">
+				{timeZones.map((t) => (
+					<TimeZone {...t} key={t.timeZone} />
+				))}
+			</div>
+		</div>
+	);
+};
